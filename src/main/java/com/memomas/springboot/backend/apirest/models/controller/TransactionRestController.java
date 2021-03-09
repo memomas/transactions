0a4cb52bebee0a042bb8f56495bec76b0227e221
Guillermo Mascote <gmascote@cinepolis.com>
2021-03-09 17:25:56 -0600

    Adding error handling to TransactionRestController

--- src/main/java/com/memomas/springboot/backend/apirest/models/controller/TransactionRestController.java
+++ src/main/java/com/memomas/springboot/backend/apirest/models/controller/TransactionRestController.java
@@ -1,12 +1,10 @@
 package com.memomas.springboot.backend.apirest.models.controller;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.memomas.springboot.backend.apirest.models.entity.Transaction;
 import com.memomas.springboot.backend.apirest.models.service.TransactionService;
 
 @RestController
@@ -17,8 +15,7 @@ public class TransactionRestController {
 	private TransactionService transactionService;
 
 	@GetMapping("/transactions/{transactionId}/user/{userId}")
-	public ResponseEntity<Transaction> findTransactionById(@PathVariable String transactionId, @PathVariable Long userId) {
-		Transaction transaction = transactionService.findTransactionByTransactionId(transactionId, userId);
-		return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
+	public ResponseEntity<?> findTransactionById(@PathVariable String transactionId, @PathVariable Long userId) {
+		return transactionService.findTransactionByTransactionId(transactionId, userId);
 	}
 }
--- src/main/java/com/memomas/springboot/backend/apirest/models/service/TransactionService.java
+++ src/main/java/com/memomas/springboot/backend/apirest/models/service/TransactionService.java
@@ -1,5 +1,11 @@
 package com.memomas.springboot.backend.apirest.models.service;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.memomas.springboot.backend.apirest.models.entity.Transaction;
@@ -10,8 +16,20 @@ public class TransactionService {
 	@Autowired
 	private TransactionRepository transactionRepository;
 	
-	public Transaction findTransactionByTransactionId(String transactionId, Long userId) {
-		Transaction transaction = transactionRepository.findTransactionByTransactionIdAndUser_UserId(transactionId, userId);
-		return transaction;
+	public ResponseEntity<?> findTransactionByTransactionId(String transactionId, Long userId) {
+		Transaction transaction = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			transaction = transactionRepository.findTransactionByTransactionIdAndUser_UserId(transactionId, userId);
+		}catch(DataAccessException e){
+			response.put("message", "Error establishing a database connection");
+			response.put("error", e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if(transaction == null) {
+			response.put("message", "Transaction not found");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
 	}
 }
