ca0f92ce331856f0b79bac2e412d7d43269989de
Guillermo Mascote <gmascote@cinepolis.com>
2021-03-09 17:54:52 -0600

    Adding error handling to TransactionService

--- src/main/java/com/memomas/springboot/backend/apirest/models/service/TransactionService.java
+++ src/main/java/com/memomas/springboot/backend/apirest/models/service/TransactionService.java
@@ -2,13 +2,11 @@ package com.memomas.springboot.backend.apirest.models.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
 import com.memomas.springboot.backend.apirest.models.entity.Transaction;
 import com.memomas.springboot.backend.apirest.models.repository.TransactionRepository;
 
@@ -35,7 +33,19 @@ public class TransactionService {
 	}
 	
 	public ResponseEntity<?> findAllTransactions(Long userId){
-		List<Transaction> transactions = transactionRepository.findAllTransactionsByUser_UserId(userId);
+		List<Transaction> transactions = null;
+		Map<String, Object> response = new HashMap<>();
+		try {
+			transactions = transactionRepository.findAllTransactionsByUser_UserId(userId);
+		}catch(DataAccessException e){
+			response.put("message", "Error establishing a database connection");
+			response.put("error", e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if(transactions.isEmpty()) {
+			response.put("message", "User does not exist");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
 		return new ResponseEntity<List<Transaction>>(transactions, HttpStatus.OK);
 	}
 }
